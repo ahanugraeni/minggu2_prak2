bcd16d0668555ef275b518508c9ad116d965cfd6
ahanugraeni <aryandari503@gmail.com>
2021-09-10 10:37:27 +0700

    membuat article controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,4 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('about', [AboutController::class, 'about']);
+Route::get('articles/{id}', [ArticleController::class, 'articles']);
